71f88db385ef0a1a8564aaa9dbe938b40939ce54
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-03-14 12:49:02 -0400

    added ajax and variables

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+var search;
+var number;
+var queryURL;
 
+queryURL= "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key="+"mhEs8PJwtIbnzwcaXFaT6AGMDE9QanMG" 
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(response) {
+      var results = response.data;
+
+      
+    });
