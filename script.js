45daddb4cb2364532218f04221696481f9efb30e
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-03-14 13:10:23 -0400

    added on click function

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 var search;
-var number;
+var number = 0;
 var queryURL;
 
-queryURL= "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key="+"mhEs8PJwtIbnzwcaXFaT6AGMDE9QanMG" 
+queryURL= "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ search + "&api-key="+"mhEs8PJwtIbnzwcaXFaT6AGMDE9QanMG" ;
 $.ajax({
     url: queryURL,
     method: "GET"
   })
     .then(function(response) {
       var results = response.data;
-
+        for (var i = 0;i < number; i++) {
+          var headline = "";
+          $("#well-section").append(headline);
+            
+        }
       
     });
+$("#run-search").on("click",function(event){
+event.preventDefault();
+search = $("#search-term").val();
+number = $("#num-records-select").val();
+console.log(search);
+console.log(number);
+})
