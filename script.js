be276ad6f32d70e5e38d164e9ee7088a9795c981
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-03-14 13:28:08 -0400

    added append function

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 var search;
-var number = 0;
+var number;
 var queryURL;
+var infodump;
 
-queryURL= "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ search + "&api-key="+"mhEs8PJwtIbnzwcaXFaT6AGMDE9QanMG" ;
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    .then(function(response) {
-      var results = response.data;
-        for (var i = 0;i < number; i++) {
-          var headline = "";
-          $("#well-section").append(headline);
-            
-        }
-      
-    });
+
 $("#run-search").on("click",function(event){
 event.preventDefault();
 search = $("#search-term").val();
 number = $("#num-records-select").val();
 console.log(search);
 console.log(number);
+queryURL= "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ search + "&api-key="+"mhEs8PJwtIbnzwcaXFaT6AGMDE9QanMG" ;
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(response) {
+       infodump = response;
+      
+        for (var i = 0;i < number; i++) {
+
+          var headline = infodump.response.docs[i].headline.main;
+          $("#well-section").append(`<h3>${headline}</h3>`);
+            
+        }
+      
+    });
 })
